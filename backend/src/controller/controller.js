3991ed055e4f47954f95a01c5e03db95b6f48c55
Justin <apolongjustin49@gmail.com>
2026-01-19 16:16:41 +0800

    feat(controller.js): add signup logic

---
+++ backend/src/controller/controller.js
@@ -0,0 +1,40 @@
+import pool from '../database/db.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+  const hashPassword = await bcrypt.hash(password, 10);
+  const avatar = 'default-1.png';
+
+  try {
+    const checkUser = await pool.query('SELECT username, email FROM users WHERE email = $1 OR username = $2;', [email, username]);
+
+    if (checkUser.rows.length > 0) {
+      const existing = checkUser.rows[0];
+
+      if (existing.email === email) {
+        return res.status(409).json({message: 'Email already in used'});
+      }
+
+      if (existing.username === username) {
+        return res.status(409).json({message: 'Username already in used'});
+      }
+    }
+
+    const result = await pool.query('INSERT INTO users (username, email, password, profile) VALUES ($1, $2, $3, $4) RETURNING *;', [username, email, hashPassword, avatar]);
+    const token = jwt.sign(
+      {userId: result.rows[0].id, email: result.rows[0].email},
+      process.env.SECRET_TOKEN,
+      {expiresIn: '1h'}
+    );
+
+    res.status(201).json({message: 'Signup successful', token});
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({message: 'Server error'});
+  }
+};
