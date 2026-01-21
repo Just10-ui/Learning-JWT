3991c00aeb6377c20966b5053d540e9faffadef0
Justin <apolongjustin49@gmail.com>
2026-01-21 10:45:20 +0800

    feat(controllerRoutes.js): added an endpoint for profile and implemented token authentication to the endpoint

--- backend/src/routes/controllerRoutes.js
+++ backend/src/routes/controllerRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { login, signup } from '../controller/controller.js';
+import { getUser, login, signup } from '../controller/controller.js';
+import { verifyToken } from '../middleware/middleware.js';
 
 const controllerRoutes = Router();
 
 controllerRoutes.post('/signup', signup);
 controllerRoutes.post('/login', login);
+controllerRoutes.get('/profile', verifyToken, getUser);
 
 export default controllerRoutes;
